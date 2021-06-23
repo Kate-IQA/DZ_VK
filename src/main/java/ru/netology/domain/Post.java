a64e33831f387b2f46acd10ebea33738bb3f1d5c
Ekaterina Degtiareva <ekaterina-kuzina-90@mail.ru>
2021-06-23 20:59:48 +0300

    first commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,29 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
+    private ReportsInfo reportsInfo;
+    private ViewsInfo viewsInfo;
+    private LikesInfo likesInfo;
+    private Geo geo;
+    private Place place;
+    // + get/set на все поля
+
+}
+
